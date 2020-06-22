049a7bba11364fdf4e701c16ee2b9f289c31c6e9
miten267 <tom6514@naver.com>
2020-06-22 16:26:23 +0900

    Allow Calculator To Input Float

--- calc/calc.py
+++ calc/calc.py
@@ -7,7 +7,7 @@ def application(environ, start_response):
 	second_num = d.get('second_num', [''])[0]
 	sum, mul = 0, 0
 	try:
-		first_num, second_num = int(first_num), int(second_num)
+		first_num, second_num = float(first_num), float(second_num)
 		sum = first_num + second_num
 		mul = first_num * second_num
 	except ValueError:
--- calc/template.py
+++ calc/template.py
@@ -2,8 +2,8 @@ html = """
 <html>
 	<body>
 		<form action="">
-				first number = <input type = "number" name = "first_num"> <br><br>
-				second number = <input type = "number" name = "second_num">
+				first number = <input type = "number" step = '0.01' name = "first_num"> <br><br>
+				second number = <input type = "number" step = '0.01' name = "second_num">
 			<input type="submit">
 		</form>
 		Sum = %(sum)s <br><br>
